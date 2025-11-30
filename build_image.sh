baafae2af9d39e491ca17b7fd197562883252ceb
Wes Kendall <wes.kendall@ambition.com>
2025-11-30 09:01:17 -0600

    Upgrade to Python 3.14, drop Python 3.9 support, add Postgres 18 support. Add support for Django 6

--- build_image.sh
+++ build_image.sh
@@ -1,1 +1,1 @@
-docker buildx build --platform linux/amd64,linux/arm64 -t opus10/circleci-python-library:${VERSION} .
+docker buildx build --platform linux/amd64,linux/arm64 -t opus10/circleci-python-library:${VERSION} --push .
